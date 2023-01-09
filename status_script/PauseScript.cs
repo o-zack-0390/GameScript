f12956c094aaf20a48eaee6ac82ddf867690df45
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-01-09 17:10:04 +0900

    Add pages

--- status_script/PauseScript.cs
+++ status_script/PauseScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 /* アイテムポーチを開いたときに一時停止させるクラス */
 public class PauseScript : MonoBehaviour {
  
-//	ポーズした時に表示するUI
+//		ポーズした時に表示するUI
 		[SerializeField]
 		private GameObject ItemScreen;
 	
@@ -12,16 +12,16 @@ public class PauseScript : MonoBehaviour {
 		{
 			if(Input.GetKeyDown ("q")) 
 			{
-	//		ポーズUIのアクティブ、非アクティブを切り替え
+	//			ポーズUIのアクティブ、非アクティブを切り替え
 				ItemScreen.SetActive (!ItemScreen.activeSelf);
  
-	//		ポーズUIが表示されてる時は停止
+	//			ポーズUIが表示されてる時は停止
 				if(ItemScreen.activeSelf) 
 				{
 					Time.timeScale = 0f;
 				}
 			
-//			ポーズUIが表示されてなければ通常通り進行
+//				ポーズUIが表示されてなければ通常通り進行
 				else 
 				{
 					Time.timeScale = 1f;
--- status_script/SlotProcess.cs
+++ status_script/SlotProcess.cs
@@ -6,23 +6,23 @@ using TMPro;
 /* スロットを管理するクラス */
 public class SlotProcess : MonoBehaviour {
 
-//	アイテムデータベース
+//		アイテムデータベース
 		public GameObject MainCamera;
 	
-//	テキスト文
+//		テキスト文
 		public TextMeshProUGUI TMP;
 	
-//	アイテムポーチに表示するアイコン
+//		アイテムポーチに表示するアイコン
 		private Sprite icon;
 		public  Image  image;
 	
-//	アイテムデータ
+//		アイテムデータ
 		private string item_name;
 		private int    item_num;
 		private string item_inf;
 
 
-//	アイテムデータをセット
+//		アイテムデータをセット
 		public void SetItem(string i_name)
 		{
 			item_name    = i_name;
@@ -34,7 +34,7 @@ public class SlotProcess : MonoBehaviour {
 		}
 	
 	
-//	アイテムデータの初期化
+//		アイテムデータの初期化
 		public void RemoveSlot()
 		{
 			item_name    = null;
@@ -47,45 +47,45 @@ public class SlotProcess : MonoBehaviour {
 		}
 	
 	
-//  マウスがスロットに重なった場合
+//  	マウスがスロットに重なった場合
  		public void MouseOver() 
 		{
-	//	テキストボックス取得
+//			テキストボックス取得
 			TMP = TMP.GetComponent<TextMeshProUGUI>();
 		
-	//	テキスト反映
+//			テキスト反映
 			TMP.text = item_name + ':' + item_num.ToString() + "\n" + item_inf;
 		}
 	
 	
-// 	アイテムを使おうとした場合
+// 		アイテムを使おうとした場合
 		public void MouseClick()
 		{
-//		アイテムが存在するなら使用
+//			アイテムが存在するなら使用
 			if(0 < item_num)
 			{
-				/*ここに使用したときの処理を記述*/
+					/*ここに使用したときの処理を記述*/
 				GetItem(item_name).SetItemNum(--item_num);
 			
-//			アイテムが無くなったらスロットを空にする
+//				アイテムが無くなったらスロットを空にする
 				if(item_num < 1)
 					RemoveSlot();
 			}
 		}
 
 
-//	マウスがスロットから離れた場合
+//		マウスがスロットから離れた場合
 		public void MouseExit() 
 		{
-//		テキスト反映
+//			テキスト反映
 			TMP.text = "Memo\n";
     }
 	
 	
-//	アイテムデータベースに同名のアイテムがあるか
+//		アイテムデータベースに同名のアイテムがあるか
 		public Item GetItem(string searchName) 
 		{
-//		アイテムデータベースから、指定されたアイテムデータを取得 ( パス : MainCamera(ゲームオブジェクト) → ItemManager(スクリプト) → ItemDataBase(アイテムデータベース) )
+//			アイテムデータベースから、指定されたアイテムデータを取得 ( パス : MainCamera(ゲームオブジェクト) → ItemManager(スクリプト) → ItemDataBase(アイテムデータベース) )
 			return MainCamera.GetComponent<ItemManager>().itemDataBase.GetItemLists().Find(itemName => itemName.GetItemName() == searchName);
 		}
 }
