aaff53c1d9bbf62dc59c1277d1f3d03580483dd4
o-zack-0390 <116938721+o-zack-0390@users.noreply.github.com>
2023-02-24 16:07:52 +0900

    Update SearchItem.cs

--- status_script/SearchItem.cs
+++ status_script/SearchItem.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 /* 周囲にアイテムがあるか判定するクラス */
 public class SearchItem : MonoBehaviour {
 	
-//	メインカメラのスクリプト「IsDestroy.cs」を参照
-	public GameObject MainCamera;
+//　 UnityChan!のスクリプト「IsDestroy.cs」を参照
+	public GameObject UnityChan;
 	
 	
 //	アイテムを拾える位置まで近づいたときに起動
@@ -16,18 +16,17 @@ public class SearchItem : MonoBehaviour {
 //		離れたオブジェクトのタグが"Item"のとき
 		if(col.tag == "Item")
 		{
-			MainCamera.GetComponent<IsDestroy>().SetItem(col.gameObject.transform.parent.gameObject);
-			MainCamera.GetComponent<IsDestroy>().ChangeFlag(1);
+			UnityChan.GetComponent<IsDestroy>().SetItem(col.gameObject.transform.parent.gameObject);
+			UnityChan.GetComponent<IsDestroy>().ChangeFlag(1);
 		}
 	}
 	
-	
 //	アイテムを拾えない位置まで離れたときに起動
 	void OnTriggerExit(Collider col)
-	{
+    {
 //		離れたオブジェクトのタグが"Item"のとき
-		if(col.tag == "Item")
-			MainCamera.GetComponent<IsDestroy>().ChangeFlag(0);
-	}
+        if(col.tag == "Item")
+            UnityChan.GetComponent<IsDestroy>().ChangeFlag(0);
+    }
  
 }
